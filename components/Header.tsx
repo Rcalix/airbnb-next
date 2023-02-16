8f70ebc194d0a16a2da0f2406c99eb36258bea74
Ramon Calix <ramoncalix@Ramons-MacBook-Pro.local>
2023-02-15 22:07:06 -0600

    add calendar function

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,25 @@
 import { NextPage } from "next";
 import Image from 'next/image'
 import { SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon, UserIcon } from '@heroicons/react/solid';
+import { useState } from "react";
+import { DateRangePicker } from 'react-date-range';
 
 const Header: NextPage = () => {
+    const [inputSearch, setInputSearch] = useState('');
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
+
+    const selectionRange ={
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection'
+    }
+
+    const handleSelect = (ranges: any) => {
+        setStartDate(ranges.selection.startDate)
+        setEndDate(ranges.selection.endDate)
+
+    }
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
             <div className="relative flex items-center h-10 cursor-pointer my-auto">
@@ -10,7 +27,7 @@ const Header: NextPage = () => {
             </div>
 
             <div className="flex items-center border-2 md:rounded-full py-2 md:shadow-sm">
-                <input className="flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search"></input>
+                <input className="flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400" onChange={(e) => setInputSearch(e.target.value)} type="text" placeholder="Start your search"></input>
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
             </div>
 
@@ -22,6 +39,9 @@ const Header: NextPage = () => {
                     <UserCircleIcon className="h-6"/>
                 </div>
             </div>
+            {inputSearch && <div className="flex flex-col col-span-3 mx-auto">
+                <DateRangePicker ranges={[selectionRange]} minDate={new Date()} rangeColors={["#FD5861"]} onChange={handleSelect}/>
+            </div>}
         </header>
         
     );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
